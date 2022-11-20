7947f8297bcf7b1726a885844dd957606bba3cb2
HayatoFunahashi <hayato_funahashi@icloud.com>
2022-11-20 23:09:41 +0900

    update

--- src/struct_def.h
+++ src/struct_def.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <fstream>
 #include <vector>
+#include <dirent.h>
 
 typedef unsigned char BYTE;
 typedef unsigned short WORD;
@@ -22,6 +23,15 @@ using namespace std;
 #define MAJOR_VER (5)
 #define MINOR_VER (1)
 #define BUILD_VER (1234)
+
+#define SIZE_WORK_BUF (1024)
+
+#define member_size_of(type, member)    sizeof(type::member)
+
+/*
+もとのソースのヘッダをそのままインクルードする
+
+*/
 
 enum eTypeDef
 {
@@ -56,9 +66,9 @@ typedef struct
 } structHead;
 
 const stParserStruct stHead[] = {
-    {eBYTE,  "major_ver", false, true},
-    {eBYTE,  "minor_ver", false, true},
-    {eWORD,  "pad",       false, true},
+    {eBYTE, "major_ver", false, true},
+    {eBYTE, "minor_ver", false, true},
+    {eWORD, "pad", false, true},
     {eDWORD, "check_sum", false, true},
 };
 
@@ -90,31 +100,81 @@ typedef struct
     stTail dummy;
 } stAllData;
 
-
 const stDefinedStruct AllSettingKas4[] = {
     ST_MEMBER_SET(stHead),
     ST_MEMBER_SET(stData),
     ST_MEMBER_SET(stEnd),
 };
 
-static char buf[1024];
+static char buf[SIZE_WORK_BUF];
 
 class BiParser
 {
 public:
-    BiParser(std::string);
-    int write(std::string, WORD, const char *);
-    int read(std::string);
-    std::vector<std::string> get_vec(void);
+    BiParser(std::string work_path)
+    {
+        const char *data_path = work_path.c_str();
+        DIR *dp;
+        dp = opendir(data_path);
+
+        dirent *entry = readdir(dp);
+        while (entry != NULL)
+        {
+            if (entry != NULL)
+            {
+                if ((string(".").compare(string(entry->d_name)) != 0) &&
+                    (string("..").compare(string(entry->d_name)) != 0))
+                {
+                    string s_path = work_path + string(entry->d_name);
+                    this->vec_path.push_back(s_path);
+                }
+            }
+            entry = readdir(dp);
+        }
+    };
+    int write(string path, WORD no, const char *moji)
+    {
+        ofstream fout;
+        structHead head = {MAJOR_VER, MINOR_VER, 0, BUILD_VER};
+        stTail tail = {1};
+
+        WORD len = strlen(moji);
+
+        fout.open(path, ios::out | ios::binary | ios::app);
+
+        if (!fout)
+        {
+            cout << path << "can not be opend";
+            return 1;
+        }
+
+        {
+            fout.write((char *)&head, sizeof(structHead));
+
+            {
+                fout.write((char *)&no, member_size_of(structData, setting_no));
+                fout.write((char *)&len, member_size_of(structData, str_len));
+                fout.write((char *)moji, len); // no NULL end
+            }
+
+            fout.write((char *)&tail, sizeof(stTail));
+        }
+
+        fout.close();
+        return 0;
+    };
     void print();
-    int parse(string path){
+    int parse(string path)
+    {
         ifstream fp(path, ios::in | ios::binary);
 
         if (!fp)
         {
             cout << path << "can not be opend";
             return 1;
-        }else{
+        }
+        else
+        {
             cout << path << endl;
         }
 
@@ -130,43 +190,48 @@ public:
                 if (p.st[k].isvariable)
                 {
                     // 可変長データなら，自分の前のメンバーを読み出してデータに変換
-                    auto d = *(size_t*)local_read(fp, p.st[k - 1].type, false);
-                    cout << "[" << d << "]" << endl;
-                    local_read(fp, d, p.st[k].isprint);
-                }else{
-                    local_read(fp, p.st[k].type, p.st[k].isprint);
+                    fp.seekg(-type2size(p.st[k - 1].type), ios_base::cur);  // shift pointer
+                    auto d = *(size_t *)impl_read(fp, p.st[k - 1].type, false);
+                    impl_read(fp, d, p.st[k].isprint);
+                }
+                else
+                {
+                    impl_read(fp, p.st[k].type, p.st[k].isprint);
                 }
             }
         }
+        fp.close();
         return 0;
     };
+
 private:
     std::vector<std::string> vec_path;
 
-    void dumpByType(void* buf, eTypeDef type){
+    void dumpByType(void *buf, eTypeDef type)
+    {
         switch (type)
         {
         case eCHAR:
-            cout << *(char*)buf << endl;
+            printf("%d\n", *(char*)buf);
             break;
         case eBYTE:
-            cout << *(BYTE*)buf << endl;
+            printf("%d\n", *(BYTE*)buf);
             break;
         case eSHORT:
-            cout << *(short*)buf << endl;
+            printf("%d\n", *(short*)buf);
             break;
         case eWORD:
-            cout << *(WORD*)buf << endl;
+            printf("%d\n", *(WORD*)buf);
             break;
         case eINT:
-            cout << *(int*)buf << endl;
+            printf("%d\n", *(int*)buf);
             break;
         case eDWORD:
-            cout << *(DWORD*)buf << endl;
+            printf("%d\n", *(DWORD*)buf);
             break;
         default:
             break;
-        }        
+        }
     }
 
     size_t type2size(eTypeDef type)
@@ -199,9 +264,9 @@ private:
         return sz;
     };
 
-    void *local_read(ifstream &fin, size_t sz, bool b_print)
+    void *impl_read(ifstream &fin, size_t sz, bool b_print)
     {
-        memset(buf, 0, 1024);
+        memset(buf, 0, SIZE_WORK_BUF);
         fin.read(buf, sz);
         if (b_print)
         {
@@ -210,9 +275,9 @@ private:
         return buf;
     };
 
-    void *local_read(ifstream &fin, eTypeDef type, bool b_print)
+    void *impl_read(ifstream &fin, eTypeDef type, bool b_print)
     {
-        memset(buf, 0, 1024);
+        memset(buf, 0, SIZE_WORK_BUF);
 
         size_t sz = type2size(type);
         fin.read(buf, sz);
